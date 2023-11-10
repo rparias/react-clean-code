e95feec08e7800ba949b3aa3f244af22d1d5bdb6
Ronald Arias <ariasron@hotmail.com>
2023-11-10 12:01:33 -0500

    adds tests cases to calculate the tip

--- src/round-up/Payment.test.tsx
+++ src/round-up/Payment.test.tsx
@@ -8,9 +8,14 @@ describe('Payment', () => {
     expect(screen.getByText(/payment/i)).toBeInTheDocument()
   })
 
-  it('Shows me the option of donate', () => {
+  it('Shows me the option of donate, case 1', () => {
     render(<Payment amount={19.9} />)
     expect(screen.getByText('I would like to donate $0.1 to charity')).toBeInTheDocument()
+  })
+
+  it('Shows me the option of donate, case 2', () => {
+    render(<Payment amount={19.8} />)
+    expect(screen.getByText('I would like to donate $0.2 to charity')).toBeInTheDocument()
   })
 
   it('Shows me the total amount', () => {
@@ -25,5 +30,14 @@ describe('Payment', () => {
 
     userEvent.click(select)
     expect(screen.getByText(/thanks for your donation/i)).toBeInTheDocument()
+  })
+
+  it('shows correct amount when user selected to donate', async () => {
+    render(<Payment amount={19.9} />)
+    const select = screen.getByText('I would like to donate $0.1 to charity')
+    expect(select).toBeInTheDocument()
+
+    userEvent.click(select)
+    expect(await screen.findByText('$20')).toBeInTheDocument()
   })
 })
--- src/round-up/Payment.tsx
+++ src/round-up/Payment.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Payment = ({ amount }: { amount: number }) => {
+  const [total, setTotal] = useState(amount)
   const [checked, setChecked] = useState(false)
-  const handleClick = () => {
+  const [tip, setTip] = useState(0)
+
+  const handleChange = () => {
     setChecked(checked => !checked)
   }
+
+  useEffect(() => {
+    setTotal(checked ? Math.floor(amount + 1) : amount)
+    setTip(parseFloat((Math.floor(amount + 1) - amount).toPrecision(2)))
+  }, [checked, amount])
 
   return (
     <div>
       <h1>Payment</h1>
       <label htmlFor="donate-checkbox">
-        <input type="checkbox" name="donate" id="donate-checkbox" onClick={handleClick} checked={checked} />
-        I would like to donate $0.1 to charity
+        <input type="checkbox" name="donate" id="donate-checkbox" onChange={handleChange} checked={checked} />
+        I would like to donate ${tip} to charity
       </label>
       {checked && <p>Thanks for your donation</p>}
       <div>
-        <button>${amount}</button>
+        <button>${total}</button>
       </div>
     </div>
   )
