37098379c83b4a2aa9127c750655318831bfffbd
Ronald Arias <ariasron@hotmail.com>
2023-11-21 11:25:07 -0500

    moves logic to roundUpLogic.ts and helpers to have clean code

--- src/round-up/helpers.ts
+++ src/round-up/helpers.ts
@@ -1,11 +1,34 @@
+import { roundUpToNearestInteger, roundUpToNearestTen, roundUpToNearestHundred } from './roundUpLogic';
 import { CountryCode } from './types';
 
 const currencyMap = {
-  JP: '¥',
+  US: '$',
   DK: 'kr.',
-  US: '$'
+  JP: '¥',
 };
+
+const algorithmMap = {
+  US: roundUpToNearestInteger,
+  DK: roundUpToNearestTen,
+  JP: roundUpToNearestHundred
+}
 
 export function getCurrencySignByCountryCode(countryCode: CountryCode) {
   return currencyMap[countryCode];
+}
+
+function formatNumber(number: number) {
+  return parseFloat(number.toPrecision(2))
+}
+
+const calculateTipFor = (calculateRoundUpFor: (amount: number) => number) => (amount: number) => {
+  return formatNumber(calculateRoundUpFor(amount) - amount)
+}
+
+export function getCalculateRoundUpFunc(countryCode: CountryCode) {
+  return algorithmMap[countryCode]
+}
+
+export function getCalculateTipFunc(countryCode: CountryCode) {
+  return calculateTipFor(algorithmMap[countryCode])
 }
--- src/round-up/useRoundUp.tsx
+++ src/round-up/useRoundUp.tsx
@@ -1,37 +1,6 @@
 import { useEffect, useState } from 'react'
 import { CountryCode } from './types'
-
-const roundUpToNearestN = (exp: number) => (amount: number) => {
-  const power = Math.pow(10, exp - 1)
-  return Math.floor(amount/power + 1) * power
-}
-
-// closure functions
-const roundUpToNearestInteger = roundUpToNearestN(1)
-const roundUpToNearestTen = roundUpToNearestN(2)
-const roundUpToNearestHundred = roundUpToNearestN(3)
-
-function formatNumber(number: number) {
-  return parseFloat(number.toPrecision(2))
-}
-
-const calculateTipFor = (calculateRoundUpFor: (amount: number) => number) => (amount: number) => {
-  return formatNumber(calculateRoundUpFor(amount) - amount);
-}
-
-const algorithmMap = {
-  US: roundUpToNearestInteger,
-  DK: roundUpToNearestTen,
-  JP: roundUpToNearestHundred
-}
-
-function getCalculateRoundUpFunc(countryCode: CountryCode) {
-  return algorithmMap[countryCode]
-}
-
-function getCalculateTipFunc(countryCode: CountryCode) {
-  return calculateTipFor(algorithmMap[countryCode])
-}
+import { getCalculateRoundUpFunc, getCalculateTipFunc } from './helpers'
 
 export const useRoundUp = (amount: number, agreeOnDonate: boolean, countryCode: CountryCode) => {
   const [total, setTotal] = useState(amount)
